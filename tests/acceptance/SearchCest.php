ad90fcb4e3df6defd360dcffb1ee97698f9c1b45
prostochizh <81864930+prostochizh@users.noreply.github.com>
2021-06-13 20:11:07 +0600

    Update SearchCest.php

--- tests/acceptance/SearchCest.php
+++ tests/acceptance/SearchCest.php
@@ -34,24 +34,9 @@ class SearchCest
     protected function getDataForSearchCarsByBody()
     {
         $carBodyTypedata = [
-            ['carBodyType' => 'sedan', 'url' => 'sedan'],
-            ['carBodyType' => 'station-wagon', 'url' => 'station-wagon'],
-            ['carBodyType' => 'hatchback', 'url' => 'hatchback'],
-            ['carBodyType' => 'limousine', 'url' => 'limousine'],
-            ['carBodyType' => 'body-coupe', 'url' => 'body-coupe'],
-            ['carBodyType' => 'body-roadster', 'url' => 'body-roadster'],
-            ['carBodyType' => 'cabriolet', 'url' => 'cabriolet'],
-            ['carBodyType' => 'suv', 'url' => 'suv'],
-            ['carBodyType' => 'crossover-suv', 'url' => 'crossover-suv'],
-            ['carBodyType' => 'microvan', 'url' => 'microvan'],
-            ['carBodyType' => 'minivan', 'url' => 'minivan'],
-            ['carBodyType' => 'van', 'url' => 'van'],
-            ['carBodyType' => 'wagon', 'url' => 'wagon'],
-            ['carBodyType' => 'body-pickup', 'url' => 'body-pickup'],
-            ['carBodyType' => 'targa', 'url' => 'targa'],
-            ['carBodyType' => 'fastback', 'url' => 'fastback'],
-            ['carBodyType' => 'liftback', 'url' => 'liftback'],
-            ['carBodyType' => 'hardtop', 'url' => 'hardtop'],
+            ['carBodyType' => 'sedan', 'url' => 'sds'],
+            ['carBodyType' => 'station-wagon', 'url' => 'statsdsdsion-wagon'],
+           
         ];
 
         return [$carBodyTypedata[array_rand($carBodyTypedata)]];
